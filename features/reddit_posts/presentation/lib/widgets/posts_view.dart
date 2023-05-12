54783e15ea38b303438e389fdceb026eb4d8ace5
0x384c0 <0x384c0@gmail.com>
2023-05-12 07:47:52 +0600

    added reddit_posts

---
+++ features/reddit_posts/presentation/lib/widgets/posts_view.dart
@@ -0,0 +1,13 @@
+library presentation;
+
+import 'package:flutter/material.dart';
+
+/// A PostsView Page.
+class PostsPage extends StatelessWidget {
+  const PostsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("PostsPage");
+  }
+}
