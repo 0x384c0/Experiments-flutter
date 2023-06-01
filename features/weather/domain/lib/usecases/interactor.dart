c44c452c516c69b930aeb161587f0f73f49836a7
0x384c0 <0x384c0@gmail.com>
2023-06-01 14:32:33 +0600

    code cleanup

--- features/weather/domain/lib/usecases/interactor.dart
+++ features/weather/domain/lib/usecases/interactor.dart
@@ -19,7 +19,7 @@ abstract class WeatherInteractor {
 class WeatherInteractorImpl extends WeatherInteractor {
   WeatherInteractorImpl(this.remoteRepository);
 
-  late RemoteRepository remoteRepository;
+  RemoteRepository remoteRepository;
 
   @override
   Future<CurrentModel> getCurrent() {
