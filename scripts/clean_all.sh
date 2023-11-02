43cdd305aa356721d1e59c21ada8cf288641e67d
0x384c0 <0x384c0@gmail.com>
2023-11-02 12:46:32 +0600

    refactored scripts

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,9 +1,14 @@
 BASEDIR=$(realpath "$(dirname "$0")"/..)
-for d in features/*/*; do
-  echo "----- Cleaning $d -----"
-  cd "$BASEDIR/$d" || exit
-  flutter clean
-done
 
-cd "$BASEDIR" || exit
-flutter clean
+function info {
+	echo "$(tput setaf 2; tput bold;)INFO: $1$(tput sgr0)"
+}
+
+# Use 'find' to locate 'pubspec.yaml' files, excluding directories starting with a dot
+for dir in $(find "$BASEDIR" -type f -name "pubspec.yaml" ! -path "*/.*" -exec dirname {} \;); do
+  # Print the current working directory
+  info "Working directory: $dir"
+
+  # Change to the directory and run 'flutter pub get'
+  (cd "$dir" && flutter clean)
+done
--- scripts/pub_get_all.sh
+++ scripts/pub_get_all.sh
@@ -1,9 +1,14 @@
 BASEDIR=$(realpath "$(dirname "$0")"/..)
-for d in features/*/*; do
-  echo "----- Running pub get for $d -----"
-  cd "$BASEDIR/$d" || exit
-  flutter pub get
-done
 
-cd "$BASEDIR" || exit
-flutter pub get
+function info {
+	echo "$(tput setaf 2; tput bold;)INFO: $1$(tput sgr0)"
+}
+
+# Use 'find' to locate 'pubspec.yaml' files, excluding directories starting with a dot
+for dir in $(find "$BASEDIR" -type f -name "pubspec.yaml" ! -path "*/.*" -exec dirname {} \;); do
+  # Print the current working directory
+  info "Working directory: $dir"
+
+  # Change to the directory and run 'flutter pub get'
+  (cd "$dir" && flutter pub get)
+done
