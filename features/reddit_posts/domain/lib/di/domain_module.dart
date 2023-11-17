570495125dcaade649775254471084f814e9ba94
0x384c0 <0x384c0@gmail.com>
2023-11-17 22:57:10 +0600

    added comment about injector

--- features/reddit_posts/domain/lib/di/domain_module.dart
+++ features/reddit_posts/domain/lib/di/domain_module.dart
@@ -4,6 +4,10 @@ import 'package:flutter_modular/flutter_modular.dart';
 class PostsDomainModule extends Module {
   @override
   exportedBinds(Injector i) {
+    // After flutter_modular:6.0.0 we cannot use PostsInteractorImpl.new, because Injector here does not contain binds from other modules
+    // Ways to get binds from other modules
+    // 1. Bypass Injector and get it from Modular.get()
+    // 2. Pass other modules in this module and put the in to List<Module> imports
     i.addSingleton<PostsInteractor>(() => PostsInteractorImpl(Modular.get()));
   }
 }
