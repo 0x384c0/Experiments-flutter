d50a30cbc8c687300e11276e32336b727010547d
0x384c0 <0x384c0@gmail.com>
2023-12-19 21:29:40 +0600

    updated environment scripts

--- scripts/ui_tests_setup.sh
+++ scripts/ui_tests_setup.sh
@@ -3,6 +3,16 @@ BASEDIR=$(realpath "$(dirname "$0")"/../ui_tests)
 
 cd $BASEDIR
 
+python3 -c 'import venv' >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "venv is already installed."
+else
+    echo "venv is not installed. Installing..."
+    python3 -m ensurepip
+    python3 -m pip install venv
+    echo "venv has been installed."
+fi
+
 # Create venv if it doesn't exist
 if [ ! -d "venv" ]; then
     echo "Creating Python virtual environment (venv)..."
--- scripts/validate_environment.sh
+++ scripts/validate_environment.sh
@@ -1,10 +1,22 @@
 # !/bin/bash
 check_command() {
-    command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 is not installed. Aborting."; exit 1; }
+  command -v $1 >/dev/null 2>&1 || {
+    echo >&2 "$1 is not installed. Aborting."
+    exit 1
+  }
 }
 
+# Build
 check_command flutter
 echo "Flutter is installed."
 
+# Upload
 check_command bundle
 echo "Bundler is installed."
+
+# UI Tests
+check_command npm
+echo "Npm is installed."
+
+check_command python3
+echo "python3 is installed."
