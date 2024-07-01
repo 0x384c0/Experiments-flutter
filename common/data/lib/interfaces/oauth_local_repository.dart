a84ebcaaae0952fcb0956923a06e424c514d2387
0x384c0 <0x384c0@gmail.com>
2024-07-01 21:54:15 +0600

    comments

--- common/data/lib/interfaces/oauth_local_repository.dart
+++ common/data/lib/interfaces/oauth_local_repository.dart
@@ -1,5 +1,6 @@
 import '../data/oauth_tokens_entity.dart';
 
+/// Data should be stored in secure storage like [BaseSecureDao]
 abstract class OAuthLocalRepository {
   Future<OauthTokensEntity?> getTokensEntity();
 
