e80ae56631a11b54d8e8c1b5a738c7b1ab174eeb
0x384c0 <0x384c0@gmail.com>
2024-07-23 21:36:53 +0600

    added mapIterable

--- common/domain/lib/mapper/mapper.dart
+++ common/domain/lib/mapper/mapper.dart
@@ -2,4 +2,6 @@ abstract class Mapper<IN, OUT> {
   OUT map(IN input);
 
   OUT? mapOptional(IN? input) => input != null ? map(input) : null;
+
+  Iterable<OUT> mapIterable(Iterable<IN> input) => input.map(map);
 }
