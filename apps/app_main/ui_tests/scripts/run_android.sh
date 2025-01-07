a72c741b7f0519d140af2eacb316bf3ffa498a58
0x384c0 <0x384c0@gmail.com>
2025-01-07 20:19:01 +0600

    refactor

--- apps/app_main/ui_tests/scripts/run_android.sh
+++ apps/app_main/ui_tests/scripts/run_android.sh
@@ -7,4 +7,4 @@ source venv/bin/activate
 
 cd appium_tests
 
-python -m pytest --app-path='../../build/app/outputs/flutter-apk/app-release.apk' -q --tb=short  -v --gherkin-terminal-reporter --disable-pytest-warnings
+python -m pytest --app-path='../../build/app/outputs/flutter-apk/app-release.apk'
--- apps/app_main/ui_tests/scripts/run_ios.sh
+++ apps/app_main/ui_tests/scripts/run_ios.sh
@@ -7,4 +7,4 @@ source venv/bin/activate
 
 cd appium_tests
 
-python -m pytest --app-path='../../build/ios/iphonesimulator/Runner.app' -q --tb=short  -v --gherkin-terminal-reporter --disable-pytest-warnings
+python -m pytest --app-path='../../build/ios/iphonesimulator/Runner.app'
