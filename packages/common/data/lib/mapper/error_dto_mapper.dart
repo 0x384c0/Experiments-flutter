931b07f6027304d91be15a3e1914b15251994efa
0x384c0 <0x384c0@gmail.com>
2025-09-24 19:39:59 +0600

    updated error mapper

--- packages/common/data/lib/mapper/error_dto_mapper.dart
+++ packages/common/data/lib/mapper/error_dto_mapper.dart
@@ -39,8 +39,18 @@ class ErrorDtoMapper extends Mapper<dynamic, ErrorModel> {
         case DioExceptionType.badCertificate:
         case DioExceptionType.unknown:
           final error = input.error;
+          String? message;
+          if (error is SocketException) {
+            message = error.message;
+          } else if (error is HttpException) {
+            message = error.message;
+          } else if (input.message?.isNotEmpty == true) {
+            message = input.message;
+          } else {
+            message = input.toString();
+          }
           return ErrorModel(
-            message: error is SocketException ? error.message : input.message,
+            message: message,
             type: ErrorModelType.unknown,
           );
       }
