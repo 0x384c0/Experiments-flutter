db589d585378f2ebe69f7711ba4c827c72b3d86a
0x384c0 <0x384c0@gmail.com>
2025-10-31 18:45:35 +0600

    updated UI libs

--- apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/app_main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import flutter_local_notifications
 import flutter_secure_storage_macos
 import geolocator_apple
 import open_file_mac
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqlite3_flutter_libs
@@ -35,6 +36,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
