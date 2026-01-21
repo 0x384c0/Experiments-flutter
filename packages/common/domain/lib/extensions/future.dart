bd3d51d496484514ae6553707c0440bd9bbb5d6f
0x384c0 <0x384c0@gmail.com>
2026-01-21 14:10:25 +0600

    refactor: keep original stack trace

--- packages/common/domain/lib/extensions/future.dart
+++ packages/common/domain/lib/extensions/future.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 
 extension MapErrorFuture<T> on Future<T> {
   Future<T> mapError<E extends Object>(E Function(dynamic error) mapError) =>
-      then<T>((value) => value, onError: (error) => throw mapError(error));
+      then<T>((value) => value, onError: (error, stackTrace) => Error.throwWithStackTrace(mapError(error), stackTrace));
 }
 
 extension NetworkCacheFuture<T> on Future<T> {
@@ -14,33 +14,28 @@ extension NetworkCacheFuture<T> on Future<T> {
     required Future Function()? invalidateCache,
   }) {
     assert((shouldInvalidateCache == null) == (invalidateCache == null));
-    return then(
-      (data) async {
+    return then((data) async {
+      try {
+        if (await shouldInvalidateCache?.call() ?? false) await invalidateCache?.call();
+        await saveToCache(data);
+      } catch (e, stacktrace) {
+        debugPrint("Error saving to cache\n$e\n$stacktrace");
         try {
-          if (await shouldInvalidateCache?.call() ?? false) await invalidateCache?.call();
-          await saveToCache(data);
+          await invalidateCache?.call();
         } catch (e, stacktrace) {
-          debugPrint("Error saving to cache\n$e\n$stacktrace");
-          try {
-            await invalidateCache?.call();
-          } catch (e, stacktrace) {
-            debugPrint("Error invalidating cache\n$e\n$stacktrace");
-          }
+          debugPrint("Error invalidating cache\n$e\n$stacktrace");
         }
-        return data;
-      },
-    ).onError<ErrorModel>(
-      (e, _) async {
-        T? result;
-        try {
-          result = await getFromCache();
-        } catch (e, stacktrace) {
-          debugPrint("Error reading from cache\n$e\n$stacktrace");
-        }
-        if (result == null) throw e;
-        return result;
-      },
-      test: (e) => e.type == ErrorModelType.connectionError,
-    );
+      }
+      return data;
+    }).onError<ErrorModel>((e, _) async {
+      T? result;
+      try {
+        result = await getFromCache();
+      } catch (e, stacktrace) {
+        debugPrint("Error reading from cache\n$e\n$stacktrace");
+      }
+      if (result == null) throw e;
+      return result;
+    }, test: (e) => e.type == ErrorModelType.connectionError);
   }
 }
